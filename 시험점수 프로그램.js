6909e8559de4aa13c052576f57bc8da57ff0cf31
Turltes And Cats <69393030+dlwnghks6821@users.noreply.github.com>
2020-09-28 09:40:20 +0900

    수우미양가 js

---
+++ 시험점수 프로그램.js
@@ -0,0 +1,18 @@
+var s = 91
+var u =81
+var m =71
+var y =61
+var g =60;
+
+var id = prompt("시험점수는?");
+if(id > s){
+  alert("수")
+}else if(id >u){
+  alert("우")
+}else if(id >m){
+  alert("미")
+}else if(id >y){
+  alert("양")
+}else if(id >g){
+  alert("가")
+}
