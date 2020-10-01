f269a5061fad82c914f17811966eb73e2421958c
Turltes And Cats <69393030+dlwnghks6821@users.noreply.github.com>
2020-10-01 15:39:45 +0900

    함수+조건문

---
+++ 자바스크립트.js
@@ -0,0 +1,11 @@
+function myFunction(){
+  var id= prompt("아이디를 입력해주세요")
+  if (id == "123qwe"){
+    document.write(id)
+    return
+     
+    var pw = prompt("비밀번호를 입력해주세요.")
+  }
+}
+
+myFunction()
