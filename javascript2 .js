08829ff800834871ebd7d0d6a202f99d0763bc13
Napalm <69393030+dlwnghks6821@users.noreply.github.com>
2020-10-29 11:30:24 +0900

    자판기

---
+++ javascript2 .js
@@ -0,0 +1,25 @@
+//주문을 받는 배열//
+var order1 =[];
+//수량을 받는 배열//
+var count1 = [];
+//배열에 값을 넣을때 사용//
+//order1.push(object);//
+
+
+alert("1.주문하기 2. 매출보기 3.나가기");
+var answer1 = prompt();
+while(answer1 !=3){
+if(answer1 ==1){
+  alert("1.a 2.b 3.c");
+  var answer2 = prompt();
+  alert("수량을 입력하세요.");
+  var answer3 = prompt();
+
+}else if(answer1 == 2){
+   alert("현재매출입니다.");
+   
+}
+   alert("1.주문하기 2. 매출보기 3.나가기");
+   answer1 = prompt();
+    }
+  
