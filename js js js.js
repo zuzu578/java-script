802e91d13dfc0720436c3831117ebe25ed4241f5
Napalm <69393030+dlwnghks6821@users.noreply.github.com>
2020-11-10 23:12:49 +0900

    Add files via upload

---
+++ js js js.js
@@ -0,0 +1,17 @@
+/*var c = {
+ running : function(args1,args2){
+   alert(args1+args2);
+ }
+}
+c.running(20);
+*/
+
+function run(name){
+ name  this
+ introducing=function(){
+   alert("my name is"+this.name)
+ }
+}
+
+var run = new run("lee");
+run.introducing();
