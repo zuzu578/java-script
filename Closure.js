6dda2da45cc0406a7907b5042406356438075969
Napalm <69393030+dlwnghks6821@users.noreply.github.com>
2020-11-11 11:57:17 +0900

    Closure

---
+++ Closure.js
@@ -0,0 +1,20 @@
+function running1(){
+  var s1 ="hello world";
+  function running2(){
+  alert(s1);
+  }
+  running2();
+}
+running1();
+
+
+
+/*function outter(){
+    var title = 'coding everybody';  
+    function inner(){        
+        alert(title);
+    }
+    inner();
+}
+outter();
+*/
